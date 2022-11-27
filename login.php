1bb94e015de1b969b93879ba4b0ca6e8311a0a91
Brian Ambani <brainambani1@gmail.com>
2022-11-27 12:53:39 +0300

    created login page linked tosign up

---
+++ login.php
@@ -0,0 +1,20 @@
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="styles.css">
+    <title>Login system</title>
+</head>
+<body>
+    <form method="post">
+        <input type="text" name="email" placeholder="Enter your username or email">
+        <input type="text" name="pwd" placeholder="Enter your password">
+        <input type="submit" name="submit" value="Log in">
+    </form>
+    <p>Register here <a href="signup.php">Sign Up</a></p>
+
+</body>
+</html>
--- signup.php
+++ signup.php
@@ -15,7 +15,8 @@
         <input type="text" name="uId" placeholder="Enter your username">
         <input type="text" name="pwd" placeholder="Enter your password">
         <input type="text" name="pwdRepeat" placeholder="Confirm your password">
-
+        <input type="submit" name="submit" value="Sign up">
     </form>
+    <p>Have an account? <a href="login.php">Log In</a></p>
 </body>
 </html>

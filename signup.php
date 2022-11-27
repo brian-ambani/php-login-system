5c70bb5c1220f1e9243bbef458086889ff28a695
Brian Ambani <brainambani1@gmail.com>
2022-11-27 12:42:44 +0300

    created form on sign up page

---
+++ signup.php
@@ -0,0 +1,21 @@
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="styles.css">
+    <title>Login system</title>
+</head>
+<body>
+    <form method="post">
+        <input type="text" name="name" placeholder="Enter your name">
+        <input type="text" name="email" placeholder="Enter your email">
+        <input type="text" name="uId" placeholder="Enter your username">
+        <input type="text" name="pwd" placeholder="Enter your password">
+        <input type="text" name="pwdRepeat" placeholder="Confirm your password">
+
+    </form>
+</body>
+</html>
